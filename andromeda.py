ff1d017ff7d889a5b46062d08111fe2e8ee2048b
Ijem <ij.ofili@gmail.com>
2015-07-26 13:36:11 +0100

    escaping chars

--- andromeda.py
+++ andromeda.py
@@ -1,10 +1,9 @@
-
 #first additions:Coming to grips with variables in python
 #someVar = 'buffer3'
 #print(someVar)
 
-#==============
+#============== use \n, \", \' to add new line, escape a double or single quote
 
 #second addition:
 someName = input('What is your name?-- ')
-print('Hi ' + someName + ', you are welcome.')
+print('Hi \'' + someName + '\', you are welcome.')
