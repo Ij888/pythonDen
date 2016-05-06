e262eadb8130b19e10cf9140c40c3c244c1b0a82
Ij888 <ij.ofili@gmail.com>
2016-05-06 23:35:23 +0100

    added entry

--- study_intro.py
+++ study_intro.py
@@ -2,4 +2,4 @@ print "Buffer Stream"
 print "Another Buffer Stream"
 print 'what do you know'
 print "Said the old code warrior, 'do not touch this'."
-print 'when "will" it grow'.
+print 'when "will" it grow.'
