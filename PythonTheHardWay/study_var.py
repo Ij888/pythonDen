0b1c1f63ac7f81e22b7b3af739c4b87d6aad4609
Ij888 <ij.ofili@gmail.com>
2016-05-07 22:55:49 +0100

    added entry

--- PythonTheHardWay/study_var.py
+++ PythonTheHardWay/study_var.py
@@ -1,4 +1,4 @@
-// 
+
 cars = 10
 space_in_a_car= 4.0
 drivers = 3
@@ -8,4 +8,9 @@ cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
-
+print cars
+print drivers
+print passengers
+print drivers
+print carpool_capacity
+print average_passengers_per_car
