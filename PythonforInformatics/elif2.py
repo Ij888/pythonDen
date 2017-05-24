8dfa8412a44951899700a1d13e1d649318e0f824
Ij888 <ij.ofili@gmail.com>
2017-05-24 14:43:39 +0100

    added entry

---
+++ PythonforInformatics/elif2.py
@@ -0,0 +1,9 @@
+R = int()
+raw_input('enter your number ')
+#change the value of R to see how the staments gets work
+if R > 10:
+	print 'we have a big number'
+elif R < 10:
+	print 'we have a little un'
+else:
+	print 'DONE!'
