ca45ec765c3284138d3053815aae5f5334b0e302
Ij888 <ij.ofili@gmail.com>
2017-07-11 11:35:42 +0100

    modified entry

--- PythonforInformatics/week3/work3c.py
+++ PythonforInformatics/week3/work3c.py
@@ -11,7 +11,7 @@ elif score >= 0.6:
     print('D')
 elif score < 0.6:
     print('F')
-catch
+except
 
 
 #3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
