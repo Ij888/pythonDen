aa1f57a2a6c04610af755dacff6ea8d9cc9bac16
Ij888 <ij.ofili@gmail.com>
2018-02-09 23:54:28 +0000

    added entry

--- PythonforInformatics/scripts.py
+++ PythonforInformatics/scripts.py
@@ -1,2 +1,10 @@
 #script that import other scripts
-def print_lyrics(): print(" some string") print(' some more string') def repeat_lyrics(): print_lyrics() print_lyrics()
+def print_lyrics(): 
+	print(" some string") 
+	print(' some more string') 
+	
+def repeat_lyrics(): 
+	print_lyrics() 
+	print_lyrics()
+	
+	repeat_lyrics()
