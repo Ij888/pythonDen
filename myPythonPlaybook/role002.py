507c1c1650d148a4af9b28e3b399943181ae1f74
Ijem <ij.ofili@gmail.com>
2018-03-26 04:27:24 +0100

    entry from 12hours back

--- myPythonPlaybook/role002.py
+++ myPythonPlaybook/role002.py
@@ -10,8 +10,8 @@ print(print_textStream)
 # def emit_sequence():
 #     field1 = ["spoofed comm networks", "compromised data packets"]
 #print (list(field1))
-#
-#
+# 
+#i think the print is not necessary. See python docs for clarification on this
 #
 #
 #
