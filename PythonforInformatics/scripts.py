b031124bff9eca6ac3d23aa3d3d0a386d0b4eec8
Ij888 <ij.ofili@gmail.com>
2018-03-26 18:28:32 +0100

    Revert "added entry"
    This reverts commit aa1f57a2a6c04610af755dacff6ea8d9cc9bac16.

--- PythonforInformatics/scripts.py
+++ PythonforInformatics/scripts.py
@@ -1,10 +1,2 @@
 #script that import other scripts
-def print_lyrics(): 
-	print(" some string") 
-	print(' some more string') 
-	
-def repeat_lyrics(): 
-	print_lyrics() 
-	print_lyrics()
-	
-	repeat_lyrics()
+def print_lyrics(): print(" some string") print(' some more string') def repeat_lyrics(): print_lyrics() print_lyrics()
