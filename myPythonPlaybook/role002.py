d962859bd2e01a49bc1f1f0f06c8f7e0985a224e
ij <ij888@ijs-MacBook-Air.local>
2018-03-26 03:51:48 +0100

    added entry

--- myPythonPlaybook/role002.py
+++ myPythonPlaybook/role002.py
@@ -1,8 +1,19 @@
 ##Functions _user defined
-def print_textStream():
+def emit_textStream():
     field1 = "spoofed comm networks"
     field2 = "compromised data packets"
     print(field2)
     print(field1)
     
-print(print_textStream)    
+print(print_textStream)
+
+# def emit_sequence():
+#     field1 = ["spoofed comm networks", "compromised data packets"]
+#print (list(field1))
+#
+#
+#
+#
+#
+#
+#
